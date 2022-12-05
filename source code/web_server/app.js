073015bc91bfecbe46f49f4797ac12bbe5adeb83
Hyeonho <mpband4869@naver.com>
2022-12-05 16:59:57 +0900

    second server

--- source code/web_server/app.js
+++ source code/web_server/app.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const app = express();
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine","ejs");
 
 
