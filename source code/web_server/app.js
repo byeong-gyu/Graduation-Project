a412cdaf6f0b6e53191870dc5097a2d7cbf437d4
hyeon4869 <119660070+hyeon4869@users.noreply.github.com>
2023-01-19 16:53:26 +0900

    server1

--- source code/web_server/app.js
+++ source code/web_server/app.js
@@ -1,42 +1,61 @@
-const express = require('express');//express 서버
-const path = require ('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const db = require('./config/mysql');
+const nunjucks = require('nunjucks');
 
+dotenv.config();
+//서버 오픈
 const app = express();
-
-app.use((req,res,next)=>{
-console.log('모든 요청 미들웨어')
-next();//exress는 위에서 내려오며 실행하지만 미들웨어는 next를 해야
-// 다음이 실행됨
-// },(req,res,next)=>{
-//     try{
-//         console.log('error 발생');
-//     } catch(error){
-//         next(error);
-//     }
- });//에러 
-
-app.set('port', process.env.PORT||3000);
-//포트를 따로 지정하지 않으면 port 값은 3000
-
-app.use('/', express.static(path.join(__dirname,'public-3030')));
-
-app.get('/', (req,res)=>{
-res.sendFile(path.join(__dirname,'../front-end/index.html'));   
+app.set('port', process.env.PORT || 3000);
+//넌적스 템플릿 엔진
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
 });
-//__dirname 절대경로 현재위치
-// app.get((err, req, res, next)=>{
-//     res.status(200).send('404 Error');
-// });// 에러 status 값을 변경
+//요청 에러 기록
+app.use(morgan('dev'));
+//실제 주소 숨기기
+app.use(express.static(path.join(__dirname, 'public')));
+//req.body 값 불러오기
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use((err,req,res,next) => {
-console.log('error'); //콘솔에는 에러를 띄우고
-res.send('404 Error');// 클라이언트 화면에는 이렇게 띄우기
+//app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser('secret'));
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+ // secret: process.env.COOKIE_SECRET,
+  secret:'secret',
+  cookie: {
+    httpOnly: true,//자바스크립트로 공격을 당하지 않게
+  },
+}));
+
+app.use('/', require('./routes/pages'));
+
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
-app.listen(app.get('port'), () =>{
-console.log('서버 실행 3000번 포트');
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});//개발일 땐 에러 상세내역 보여주기, 배포 모드일 땐 감추기
+
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
 // app.get('port') port 값을 받은 후 
 // 서버 열기, 열렸다면 서버 실행을 콘솔창에 출력
-
-
