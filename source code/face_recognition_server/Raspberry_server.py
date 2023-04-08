f836c66401ee4cbfa842b10df2f2a9d2d2da801f
byeong-gyu2 <119674687+byeong-gyu2@users.noreply.github.com>
2023-04-08 23:20:06 +0900

    [update] 라즈베리파이 테스트 코드

--- source code/face_recognition_server/Raspberry_server.py
+++ source code/face_recognition_server/Raspberry_server.py
@@ -1,4 +1,7 @@
+import cv2
+import numpy as np
 import socket
+import struct
 
 # 서버 정보
 server_ip = '127.0.0.1'
@@ -7,34 +10,41 @@ server_port = 5000
 # 소켓 생성
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 서버 바인딩
+# 소켓 바인드
 server_socket.bind((server_ip, server_port))
 
-# 클라이언트 연결 대기
+# 연결 대기
 server_socket.listen()
 
 # 클라이언트 연결 수락
 client_socket, addr = server_socket.accept()
 
+# 이미지 수신 및 출력
 while True:
-    # 송신된 이미지 데이터 수신
-    data = b''
-    while True:
-        packet = client_socket.recv(1024)
-        if not packet:
-            break
-        data += packet
+    # 이미지 데이터 크기 수신
+    data = b""
+    payload_size = struct.calcsize(">I")
+    while len(data) < payload_size:
+        data += client_socket.recv(4096)
 
-    # 수신된 데이터를 이미지로 변환
-    img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+    # 이미지 데이터 수신
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">I", packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
 
-    # 수신된 이미지 출력
-    cv2.imshow('received image', img)
+    # 이미지 데이터 배열 변환
+    frame_data = data[:msg_size]
+    frame = np.frombuffer(frame_data, dtype=np.uint8)
+    frame = frame.reshape(-1, 150, 150, 3)
 
-    # q 키를 누르면 종료
+    # 이미지 출력
+    cv2.imshow('Face Detection', frame[0])
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# 소켓 닫기
+# 소켓 및 카메라 리소스 해제
 client_socket.close()
 server_socket.close()
+cv2.destroyAllWindows()
