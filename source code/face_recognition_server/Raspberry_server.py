42b26e5514077c8afaf1dfdd6982241b8ed9030e
byeong-gyu2 <119674687+byeong-gyu2@users.noreply.github.com>
2023-04-09 13:14:09 +0900

    [update] 라즈베리파이 서버 테스트 코드

--- source code/face_recognition_server/Raspberry_server.py
+++ source code/face_recognition_server/Raspberry_server.py
@@ -1,3 +1,4 @@
+# 서버 소스코드
 import cv2
 import numpy as np
 import socket
@@ -7,36 +8,47 @@ import struct
 server_ip = '127.0.0.1'
 server_port = 5000
 
-# 소켓 생성 및 바인딩
+# 소켓 생성
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 서버 바인딩
 server_socket.bind((server_ip, server_port))
 
-# 클라이언트 연결 대기
-server_socket.listen(1)
+# 서버 리스닝
+server_socket.listen()
+
+# 클라이언트 연결 수락
+print('Waiting for client...')
 client_socket, addr = server_socket.accept()
+print('Client connected:', addr)
 
 # 이미지 수신 함수
-def recv_image():
+def receive_image():
     # 이미지 데이터 크기 수신
-    size_data = b''
-    while len(size_data) < 4:
-        size_data += client_socket.recv(4 - len(size_data))
+    size_data = client_socket.recv(4)
     size = struct.unpack('>L', size_data)[0]
 
     # 이미지 데이터 수신
-    frame_data = b''
-    while len(frame_data) < size:
-        frame_data += client_socket.recv(size - len(frame_data))
+    img_data = b''
+    while len(img_data) < size:
+        data = client_socket.recv(size - len(img_data))
+        if not data:
+            return None
+        img_data += data
 
-    # 수신한 데이터 배열 형태 변환
-    frame = np.frombuffer(frame_data, dtype=np.uint8)
-    frame = np.reshape(frame, (150, 150, 3))
+    # 이미지 배열로 변환
+    img = np.frombuffer(img_data, dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
 
-    return frame
+    return img
 
 while True:
-    # 이미지 수신
-    frame = recv_image()
+    # 클라이언트로부터 이미지 수신
+    frame = receive_image()
+
+    # 수신된 이미지가 없으면 종료
+    if frame is None:
+        break
 
     # 이미지 출력
     cv2.imshow('Received Image', frame)
